3000240a6a1e8e79c310adf69ea67c5809ae0ed7
Sam <super8989@gmail.com>
2020-09-17 21:24:03 -0400

    Feature: User Authentication

--- config/routes.rb
+++ config/routes.rb
@@ -14,6 +14,16 @@ Rails.application.routes.draw do
 
   resources :orders, only: [:create, :show]
 
+  # resources :users
+
+  get '/login' => 'sessions#new'
+  post '/login' => 'sessions#create'
+
+  get '/logout' => 'sessions#destroy'
+
+  get '/signup' => 'users#new'
+  post '/users' => 'users#create'
+
   namespace :admin do
     root to: 'dashboard#show'
     resources :products, except: [:edit, :update, :show]
